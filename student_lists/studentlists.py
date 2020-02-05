2d10743412546e5cf9833489067ad0fff5338f17
B33 <my@email.com>
2020-02-04 19:18:53 -0600

    Add is_class_full method.

--- student_lists/studentlists.py
+++ student_lists/studentlists.py
@@ -48,8 +48,11 @@ class ClassList:
         return None
 
 
-    ## TODO add a method called is_class_full.
-    # This should return True or False to indicate if the class is full.
+    def is_class_full(self):
+        if len(self.class_list) >= self.max_students:
+            return True
+        else:
+            return False
 
 
     def __str__(self):
